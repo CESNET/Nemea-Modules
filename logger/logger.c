cbaa52e72127df043a5e1a1f78cebb985a489b09
Vaclav Bartos <washek@cesnet.cz>
2013-08-20 16:18:58 +0200

    Nemea Logger: final version of Logger module

--- logger/logger.c
+++ logger/logger.c
@@ -44,10 +44,11 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <getopt.h>
+#include <time.h>
 #include <libtrap/trap.h>
 #include "../../unirec/unirec.h"
 #include <omp.h>
-#include <unistd.h>
+#include "../../common/time_convert/time_convert.h" // TODO this should be part of unirec
 
 // Struct with information about module
 trap_module_info_t module_info = {
@@ -68,12 +69,12 @@ static int stop = 0;
 int verbose;
 static int n_inputs; // Number of input interfaces
 static ur_template_t **templates; // UniRec templates of input interfaces (array of length n_inputs)
-//char **files; // Names of files to write records into (array of length n_inputs)
+static ur_template_t *out_template; // UniRec template with union of fields of all inputs
+int print_ifc_num = 0;
 
 static FILE *file; // Output file
 
 TRAP_DEFAULT_SIGNAL_HANDLER(stop = 1);
-
 
 void capture_thread(int index)
 {
@@ -104,6 +105,9 @@ void capture_thread(int index)
       // Check size of received data
       if (rec_size < ur_rec_static_size(templates[index])) {
          if (rec_size <= 1) {
+            if (verbose >= 0) {
+               printf("Interface %i received ending record, the interface will be closed.\n", index, rec_size);
+            }
             break; // End of data (used for testing purposes)
          } else {
             fprintf(stderr, "Error: data with wrong size received (expected size: >= %hu, received size: %hu)\n",
@@ -112,9 +116,101 @@ void capture_thread(int index)
          }
       }
       
+      // Print contents of received UniRec to output
       #pragma omp critical
       {
-         fprintf(file,"%i", index);
+         if (print_ifc_num) {
+            fprintf(file,"%i,", index);
+         }
+         // Iterate over all output fields
+         int comma = 0;
+         ur_field_id_t id = UR_INVALID_FIELD;
+         while((id = ur_iter_fields(out_template, id)) != UR_INVALID_FIELD) {
+            if (comma) {
+               fprintf(file,",");
+            }
+            comma = 1;
+            if (ur_is_present(templates[index], id)) {
+               // Print the field
+               if (ur_is_dynamic(id)) {
+                  // Dynamic field - write as a string
+                  int size = ur_get_dyn_size(templates[index], rec, id);
+                  char *data = ur_get_dyn(templates[index], rec, id);
+                  fprintf(file, "\"");
+                  while (size--) {
+                     fprintf(file, "%c", *(data++));
+                  }
+                  fprintf(file, "\"");
+               } else {
+                  // Static field - check what type is it and use appropriate format
+                  void *ptr = ur_get_ptr_by_id(templates[index], rec, id);
+                  switch (ur_get_type_by_id(id)) {
+                     case UR_TYPE_UINT8:
+                        fprintf(file, "%u", *(uint8_t*)ptr);
+                        break;
+                     case UR_TYPE_UINT16:
+                        fprintf(file, "%u", *(uint16_t*)ptr);
+                        break;
+                     case UR_TYPE_UINT32:
+                        fprintf(file, "%u", *(uint32_t*)ptr);
+                        break;
+                     case UR_TYPE_UINT64:
+                        fprintf(file, "%lu", *(uint64_t*)ptr);
+                        break;
+                     case UR_TYPE_INT8:
+                        fprintf(file, "%i", *(int8_t*)ptr);
+                        break;
+                     case UR_TYPE_INT16:
+                        fprintf(file, "%i", *(int16_t*)ptr);
+                        break;
+                     case UR_TYPE_INT32:
+                        fprintf(file, "%i", *(int32_t*)ptr);
+                        break;
+                     case UR_TYPE_INT64:
+                        fprintf(file, "%li", *(int64_t*)ptr);
+                        break;
+                     case UR_TYPE_CHAR:
+                        fprintf(file, "%c", *(char*)ptr);
+                        break;
+                     case UR_TYPE_FLOAT:
+                        fprintf(file, "%f", *(float*)ptr);
+                        break;
+                     case UR_TYPE_DOUBLE:
+                        fprintf(file, "%f", *(double*)ptr);
+                        break;
+                     case UR_TYPE_IP:
+                        {
+                           // IP address - convert to human-readable format and print
+                           char str[46];
+                           ip_to_str((ip_addr_t*)ptr, str);
+                           fprintf(file, "%s", str);
+                        }
+                        break;
+                     case UR_TYPE_TIME:
+                        {
+                           // Timestamp - convert to human-readable format and print
+                           time_t sec = TIME_CONVERT_UNIREC_TO_SECS(*(ur_time_t*)ptr);
+                           int msec = TIME_CONVERT_UNIREC_TO_MSECS(*(ur_time_t*)ptr);
+                           char str[32];
+                           strftime(str, 31, "%FT%T", gmtime(&sec));
+                           fprintf(file, "%s.%03i", str, msec);
+                        }
+                        break;
+                     default:
+                        {
+                           // Unknown type - print the value in hex
+                           int size = ur_get_size_by_id(id);
+                           fprintf(file, "0x");
+                           for (int i = 0; i < size; i++) {
+                              fprintf(file, "%02x", ((unsigned char*)ptr)[i]);
+                           }
+                        }
+                        break;
+                  } // case (field type)
+               } // if (not) dynamic
+            } // if present
+         } // loop over fields
+         fprintf(file,"\n");
          fflush(file);
       }
    }
@@ -128,7 +224,10 @@ void capture_thread(int index)
 int main(int argc, char **argv)
 {
    int ret;
+   char *out_template_str = NULL;
    char *out_filename = NULL;
+   int append = 0;
+   int print_title = 0;
    
    // ***** Process parameters *****
    
@@ -151,28 +250,44 @@ int main(int argc, char **argv)
    
    // Parse remaining parameters and get configuration
    char opt;
-   while ((opt = getopt(argc, argv, "w:")) != -1) {
+   while ((opt = getopt(argc, argv, "w:a:o:tn")) != -1) {
       switch (opt) {
+         case 'a':
+            append = 1;
+            // continue below...
          case 'w':
+            if (out_filename != NULL) { // Output file is already specified
+               fprintf(stderr, "Error: Only one output file may be specified.\n");
+               return 1;
+            }
             out_filename = optarg;
             break;
+         case 'o':
+            out_template_str = optarg;
+            break;
+         case 't':
+            print_title = 1;
+            break;
+         case 'n':
+            print_ifc_num = 1;
+            break;
          default:
-            fprintf(stderr, "Invalid arguments.\n");
+            fprintf(stderr, "Error: Invalid arguments.\n");
             return 1;
       }
    }
    
    // Create UniRec templates
-   n_inputs = argc - 1;
+   n_inputs = argc - optind;
    if (verbose >= 0) {
       printf("Number of inputs: %i\n", n_inputs);
    }
    if (n_inputs < 1) {
-      fprintf(stderr, "You must specify at least one UniRec template.\n");
+      fprintf(stderr, "Error: You must specify at least one UniRec template.\n");
       return 0;
    }
    if (n_inputs > 32) {
-      fprintf(stderr, "More than 32 interfaces is not allowed by TRAP library.\n");
+      fprintf(stderr, "Error: More than 32 interfaces is not allowed by TRAP library.\n");
       return 4;
    }
    
@@ -186,19 +301,40 @@ int main(int argc, char **argv)
    }
    
    for (int i = 0; i < n_inputs; i++) {
-      templates[i] = ur_create_template(argv[i+1]);
+      templates[i] = ur_create_template(argv[i+optind]);
       if (templates[i] == NULL) {
-         fprintf(stderr, "Invalid template: %s\n", argv[i+1]);
+         fprintf(stderr, "Error: Invalid template: %s\n", argv[i+optind]);
+         while (i--) {
+            ur_free_template(templates[i]);
+         }
          free(templates);
          return 2;
       }
    }
    
-   // Set number of input interfaces
-   module_info.num_ifc_in = n_inputs;
+   // Create output UniRec template (user-specified or union of all inputs)
+   if (out_template_str == NULL) {
+      // Create output template as a union of all input templates
+      out_template = ur_union_templates(templates, n_inputs);
+      if (out_template == NULL) {
+         fprintf(stderr, "Memory allocation error\n");
+         ret = -1;
+         goto exit;
+      }
+   } else {
+      out_template = ur_create_template(out_template_str);
+      if (out_template == NULL) {
+         fprintf(stderr, "Error: Invalid template: %s\n", out_template_str);
+         ret = -1;
+         goto exit;
+      }
+   }
    
    
    // ***** TRAP initialization *****
+   
+   // Set number of input interfaces
+   module_info.num_ifc_in = n_inputs;
    
    if (verbose >= 0) {
       printf("Initializing TRAP library ...\n");
@@ -208,9 +344,9 @@ int main(int argc, char **argv)
    ret = trap_init(&module_info, ifc_spec);
    if (ret != TRAP_E_OK) {
       fprintf(stderr, "ERROR in TRAP initialization: %s\n", trap_last_error_msg);
-      free(templates);
       trap_free_ifc_spec(ifc_spec);
-      return 2;
+      ret = 2;
+      goto exit;
    }
    
    // We don't need ifc_spec anymore, destroy it
@@ -230,8 +366,8 @@ int main(int argc, char **argv)
       file = fopen(out_filename, "w");
       if (file == NULL) {
          perror("Error: can't open output file:");
-         free(templates);
-         return 3;
+         ret = 3;
+         goto exit;
       }
    } else {
       file = stdout;
@@ -241,19 +377,40 @@ int main(int argc, char **argv)
       printf("Initialization done.\n");
    }
    
+   // Print a header - names of output UniRec fields
+   if (print_title) {
+      if (print_ifc_num) {
+         fprintf(file, "IFC,");
+      }
+      int comma = 0;
+      ur_field_id_t id = UR_INVALID_FIELD;
+      while((id = ur_iter_fields(out_template, id)) != UR_INVALID_FIELD) {
+         if (comma) {
+            fprintf(file, ",");
+         }
+         fprintf(file, "%s", ur_get_name_by_id(id));
+         comma = 1;
+      }
+      fprintf(file, "\n");
+   }
    
    // ***** Start a thread for each interface *****
-   
+
    #pragma omp parallel num_threads(n_inputs)
    {
       capture_thread(omp_get_thread_num());
    }
    
+   ret = 0;
+   
    // ***** Cleanup *****
 
+exit:
    if (verbose >= 0) {
-      printf("Exitting (performing cleanup) ...\n");
+      printf("Exitting ...\n");
    }
+   
+   trap_terminate(); // This have to be called before trap_finalize(), otherwise it may crash (don't know if feature or bug in TRAP)
    
    // Do all necessary cleanup before exiting
    TRAP_DEFAULT_FINALIZATION();
@@ -261,11 +418,9 @@ int main(int argc, char **argv)
    for (int i = 0; i < n_inputs; i++) {
       ur_free_template(templates[i]);
    }
+   free(templates);
+   ur_free_template(out_template);
    
-   if (verbose >= 0) {
-      printf("Exitting...\n");
-   }
-   
-   return 0;
+   return ret;
 }
 
