23e449410a791744079daa216ceb1b985775b6d4
Pavel Krobot <xkrobo01@cesnet.cz>
2014-07-03 15:07:46 +0200

    Merger: Code cleanup, README update, etc. (No functionality changes).

--- merger/merger.c
+++ merger/merger.c
@@ -1,11 +1,11 @@
 /**
  * \file merger.c
- * \brief Merge traffic incoming on mutiple interfaces.
+ * \brief Merge traffic incoming on mutiple interfaces into one output stream.
  * \author Pavel Krobot <xkrobo01@cesnet.cz>
- * \date 2014
+ * \date 3/2014
  */
 /*
- * Copyright (C) 2013 CESNET
+ * Copyright (C) 2014 CESNET
  *
  * LICENSE TERMS
  *
@@ -51,21 +51,16 @@
 #include <libtrap/trap.h>
 #include <unirec/unirec.h>
 
-#define TS_LAST 	0
-#define TS_FIRST	1
+#define TS_LAST   0
+#define TS_FIRST  1
 
-#define INACTIVE_USLEEP_TIME	100000
-#define DEFAULT_TIMEOUT			1000000
-//#define DEFAULT_TIMEOUT		TRAP_WAIT
-#define TIME_DIFF_SLEEP		5
+#define INACTIVE_USLEEP_TIME  100000
+#define DEFAULT_TIMEOUT       1000000
+//#define DEFAULT_TIMEOUT     TRAP_WAIT
+#define TIME_DIFF_SLEEP    5
 
-#define MODE_TIME_IGNORE	0
-#define MODE_TIME_AWARE		1
-
-//#define DEBUG
-//#ifdef DEBUG
-////	#include <iomanip>
-//#endif
+#define MODE_TIME_IGNORE   0
+#define MODE_TIME_AWARE    1
 
 // Struct with information about module
 trap_module_info_t module_info = {
@@ -104,10 +99,10 @@ static int verbose;
 //TRAP_DEFAULT_SIGNAL_HANDLER(stop = 1);
 void signal_handler(int signal)
 {
-	if (signal == SIGTERM || signal == SIGINT) {
-		stop = 1;
-		trap_terminate();
-	}
+   if (signal == SIGTERM || signal == SIGINT) {
+      stop = 1;
+      trap_terminate();
+   }
 }
 
 static int timestamp_selector = TS_LAST; // Tells to sort timestamps based on TIME_FIRST or TIME_LAST field
@@ -120,12 +115,17 @@ static ur_template_t *out_template; // UniRec template of output interface
 static int active_interfaces;
 static int initial_timeout = DEFAULT_TIMEOUT; // Initial timeout for incoming interfaces (in miliseconds)
 
+/**
+ * Timestamp-aware capture thread - handles data incomming from on one interface.
+ *
+ * @param [in] index Index of the given link.
+ */
 void ta_capture_thread(int index)
 {
-	int private_stop = 0;
+   int private_stop = 0;
    int ret;
 
-	int outage_flag = 0;
+   int outage_flag = 0;
    int read_next = 1;
    int timeout = initial_timeout;
    ur_time_t rec_time;
@@ -138,136 +138,141 @@ void ta_capture_thread(int index)
 
    // Read data from input and log them to a file
    while (!stop && !private_stop) {
-   	if (stop) private_stop = stop;
+      if (stop) private_stop = stop;
 
-		const void *rec;
-		uint16_t rec_size;
+      const void *rec;
+      uint16_t rec_size;
 
-		if (read_next){
-			rec_time = 0;
+      if (read_next){
+         rec_time = 0;
 
-			if (verbose >= 2) {
-				printf("Thread %i: calling trap_recv()\n", index);
-			}
-			// Receive data from index-th input interface, wait until data are available
-			ret = trap_recv(index, &rec, &rec_size);
+         if (verbose >= 2) {
+            printf("Thread %i: calling trap_recv()\n", index);
+         }
+         // Receive data from index-th input interface, wait until data are available
+         ret = trap_recv(index, &rec, &rec_size);
 
-			if (ret != TRAP_E_OK) {
-				if (ret == TRAP_E_TIMEOUT) {//input probably (temporary) offline
-					if (verbose >= 0) {
-						printf("Thread %i: no data received (timeout %u).\n", index, timeout);
-					}
-				} else if (ret == TRAP_E_TERMINATED) {// Module was terminated while waiting for new data (e.g. by Ctrl-C)
-					private_stop = 1;
-				} else {
-					// Some error has occured
-					if (verbose >= 0) {
-						fprintf(stderr, "Error: trap_get_data() returned %i (%s)\n", ret, trap_last_error_msg);
-					}
-				}
-				if(!outage_flag){
-					outage_flag = 1;
-					trap_ifcctl(TRAPIFC_INPUT, index, TRAPCTL_SETTIMEOUT, TRAP_NO_WAIT);
-					#pragma omp atomic
-					--active_interfaces;
-				}
-			} else {
-				if (verbose >= 2) {
-					printf("Thread %i: received %hu bytes of data\n", index, rec_size);
-				}
-				// Check size of received data
-				if (rec_size < ur_rec_static_size(in_template)) {
-					if (rec_size <= 1) {
-						if (verbose >= 0) {
-							printf("Interface %i received ending record, the interface will be closed.\n", index, rec_size);
-						}
-//						read_next = 0;
-					} else {
-						if (verbose >= 0) {
-							fprintf(stderr, "Error: data with wrong size received (expected size: >= %hu, received size: %hu)\n",
-								  ur_rec_static_size(in_template), rec_size);
-						}
-					}
-					if(!outage_flag){
-						outage_flag = 1;
-						trap_ifcctl(TRAPIFC_INPUT, index, TRAPCTL_SETTIMEOUT, TRAP_NO_WAIT);
-						#pragma omp atomic
-						--active_interfaces;
-					}
-				} else {
-					if (outage_flag) {
-						outage_flag = 0;
-						trap_ifcctl(TRAPIFC_INPUT, index, TRAPCTL_SETTIMEOUT, timeout);
-						#pragma omp atomic
-						++active_interfaces;
-					}
-					if (timestamp_selector == TS_FIRST){
-						rec_time = ur_get(in_template, rec, UR_TIME_FIRST);
-					} else {
-						rec_time = ur_get(in_template, rec, UR_TIME_LAST);
-					}
-					read_next = 0;
-				}
-			}
-		}
+         if (ret != TRAP_E_OK) {
+            if (ret == TRAP_E_TIMEOUT) {//input probably (temporary) offline
+               if (verbose >= 0) {
+                  printf("Thread %i: no data received (timeout %u).\n", index, timeout);
+               }
+            } else if (ret == TRAP_E_TERMINATED) {// Module was terminated while waiting for new data (e.g. by Ctrl-C)
+               private_stop = 1;
+            } else {
+               // Some error has occured
+               if (verbose >= 0) {
+                  fprintf(stderr, "Error: trap_get_data() returned %i (%s)\n", ret, trap_last_error_msg);
+               }
+            }
+            if(!outage_flag){
+               outage_flag = 1;
+               trap_ifcctl(TRAPIFC_INPUT, index, TRAPCTL_SETTIMEOUT, TRAP_NO_WAIT);
+               #pragma omp atomic
+               --active_interfaces;
+            }
+         } else {
+            if (verbose >= 2) {
+               printf("Thread %i: received %hu bytes of data\n", index, rec_size);
+            }
+            // Check size of received data
+            if (rec_size < ur_rec_static_size(in_template)) {
+               if (rec_size <= 1) {
+                  if (verbose >= 0) {
+                     printf("Interface %i received ending record, the interface will be closed.\n", index, rec_size);
+                  }
+//                read_next = 0;
+               } else {
+                  if (verbose >= 0) {
+                     fprintf(stderr, "Error: data with wrong size received (expected size: >= %hu, received size: %hu)\n",
+                          ur_rec_static_size(in_template), rec_size);
+                  }
+               }
+               if(!outage_flag){
+                  outage_flag = 1;
+                  trap_ifcctl(TRAPIFC_INPUT, index, TRAPCTL_SETTIMEOUT, TRAP_NO_WAIT);
+                  #pragma omp atomic
+                  --active_interfaces;
+               }
+            } else {
+               if (outage_flag) {
+                  outage_flag = 0;
+                  trap_ifcctl(TRAPIFC_INPUT, index, TRAPCTL_SETTIMEOUT, timeout);
+                  #pragma omp atomic
+                  ++active_interfaces;
+               }
+               if (timestamp_selector == TS_FIRST){
+                  rec_time = ur_get(in_template, rec, UR_TIME_FIRST);
+               } else {
+                  rec_time = ur_get(in_template, rec, UR_TIME_LAST);
+               }
+               read_next = 0;
+            }
+         }
+      }
 
-		#pragma omp barrier
+      #pragma omp barrier
 
-		if (!outage_flag){
-			#pragma omp critical (minimum)
-			{
-				if (actual_min_timestamp == 0 || actual_min_timestamp > rec_time){
-					actual_min_timestamp = rec_time;
-				} else {
-		//			sleep(rec_time - actual_min_timestamp);
-				}
-			}
-		}
+      if (!outage_flag){
+         #pragma omp critical (minimum)
+         {
+            if (actual_min_timestamp == 0 || actual_min_timestamp > rec_time){
+               actual_min_timestamp = rec_time;
+            } else {
+      //       sleep(rec_time - actual_min_timestamp);
+            }
+         }
+      }
 
-		#pragma omp barrier
+      #pragma omp barrier
 
-		if (!outage_flag){
-			if (actual_min_timestamp == rec_time){
-				#pragma omp critical (sending)
-				{
-					ret = trap_send(0, rec, rec_size);
+      if (!outage_flag){
+         if (actual_min_timestamp == rec_time){
+            #pragma omp critical (sending)
+            {
+               ret = trap_send(0, rec, rec_size);
 
-					if (ret != TRAP_E_OK) {
-						if (ret == TRAP_E_TERMINATED) {
-							private_stop = 1; // Module was terminated while waiting for new data (e.g. by Ctrl-C)
-							#pragma omp atomic
-							--active_interfaces;
-						} else {
-							// Some error has occured
-							if (verbose >= 0) {
-								fprintf(stderr, "Error: trap_send_data() returned %i (%s)\n", ret, trap_last_error_msg);
-								fprintf(stderr, "   Message skipped...\n");
-							}
-							read_next = 1;
-						}
-					} else {
-						read_next = 1;
-						actual_min_timestamp = 0;
-					}
-				}
-			}
-		}
+               if (ret != TRAP_E_OK) {
+                  if (ret == TRAP_E_TERMINATED) {
+                     private_stop = 1; // Module was terminated while waiting for new data (e.g. by Ctrl-C)
+                     #pragma omp atomic
+                     --active_interfaces;
+                  } else {
+                     // Some error has occured
+                     if (verbose >= 0) {
+                        fprintf(stderr, "Error: trap_send_data() returned %i (%s)\n", ret, trap_last_error_msg);
+                        fprintf(stderr, "   Message skipped...\n");
+                     }
+                     read_next = 1;
+                  }
+               } else {
+                  read_next = 1;
+                  actual_min_timestamp = 0;
+               }
+            }
+         }
+      }
 
-		if (!active_interfaces){
-			trap_ifcctl(TRAPIFC_INPUT, index, TRAPCTL_SETTIMEOUT, timeout);
-//			usleep(INACTIVE_USLEEP_TIME);
-//			private_stop = 1;
-		}
+      if (!active_interfaces){
+         trap_ifcctl(TRAPIFC_INPUT, index, TRAPCTL_SETTIMEOUT, timeout);
+//       usleep(INACTIVE_USLEEP_TIME);
+//       private_stop = 1;
+      }
    } // end while(!stop && !private_stop)
 
    if (verbose >= 1) {
-		printf("Thread %i exitting.\n", index);
+      printf("Thread %i exitting.\n", index);
    }
 }
 
+/**
+ * Basic capture thread - handles data incomming from on one interface.
+ *
+ * @param [in] index Index of the given link.
+ */
 void capture_thread(int index)
 {
-	int private_stop = 0;
+   int private_stop = 0;
    int ret;
 //   int timeout = initial_timeout;
 
@@ -280,84 +285,65 @@ void capture_thread(int index)
 
    // Read data from input and log them to a file
    while (!stop && !private_stop) {
-		const void *rec;
-		uint16_t rec_size;
+      const void *rec;
+      uint16_t rec_size;
 
-		if (verbose >= 2) {
-			printf("Thread %i: calling trap_recv()\n", index);
-		}
-		// Receive data from index-th input interface, wait until data are available
-		ret = trap_recv(index, &rec, &rec_size);
-		TRAP_DEFAULT_GET_DATA_ERROR_HANDLING(ret, continue, break);
+      if (verbose >= 2) {
+         printf("Thread %i: calling trap_recv()\n", index);
+      }
+      // Receive data from index-th input interface, wait until data are available
+      ret = trap_recv(index, &rec, &rec_size);
+      TRAP_DEFAULT_GET_DATA_ERROR_HANDLING(ret, continue, break);
 
-		if (verbose >= 2) {
-			printf("Thread %i: received %hu bytes of data\n", index, rec_size);
-		}
+      if (verbose >= 2) {
+         printf("Thread %i: received %hu bytes of data\n", index, rec_size);
+      }
 
-		// Check size of received data
-		if (rec_size < ur_rec_static_size(in_template)) {
-			if (rec_size <= 1) {
-				if (verbose >= 0) {
-					printf("Interface %i received ending record, the interface will be closed.\n", index, rec_size);
-				}
-				private_stop = 1;
-				if (--active_interfaces > 0){// Only last thread send terminating message.
-					break;
-				}
-			} else {
-				fprintf(stderr, "Error: data with wrong size received (expected size: >= %hu, received size: %hu)\n",
-						  ur_rec_static_size(in_template), rec_size);
-				break;
-			}
-		}
+      // Check size of received data
+      if (rec_size < ur_rec_static_size(in_template)) {
+         if (rec_size <= 1) {
+            if (verbose >= 0) {
+               printf("Interface %i received ending record, the interface will be closed.\n", index, rec_size);
+            }
+            private_stop = 1;
+            if (--active_interfaces > 0){// Only last thread send terminating message.
+               break;
+            }
+         } else {
+            fprintf(stderr, "Error: data with wrong size received (expected size: >= %hu, received size: %hu)\n",
+                    ur_rec_static_size(in_template), rec_size);
+            break;
+         }
+      }
 
-		if ((rec_size < ur_rec_static_size(in_template))) {
-			if (rec_size <= 1) { // end of data
-				break;
-			} else { // data corrupted
-				fprintf(stderr,"Error: Wrong data size. Expected: %i", ur_rec_static_size(in_template));
-				fprintf(stderr," Recieved: %i.\n", rec_size);
-//				ifstream fx("wrong-packets.log");
-//				if (fx.is_open()){
-//					unsigned char const* bytes = static_cast<unsigned char const*>(rec);
-//					for (int i = 0; i < rec_size; ++i){
-//						fx << std::hex << static_cast<unsigned int> (bytes[i]);
-//					}
-////				char buff [INET6_ADDRSTRLEN];
-////				ip_to_str(ur_get_ptr(in_template, rec, UR_SRC_IP), buff);
-////				cout << buff << ":" << ur_get(in_template, rec, UR_DST_PORT);
-////				ip_to_str(ur_get_ptr(in_template, rec, UR_DST_IP), buff);
-////				cout << " -> " << buff << ":" << ur_get(in_template, rec, UR_DST_PORT);
-////				cout << "; " << ur_get(in_template, rec, UR_PROTOCOL) << "; " << ur_get(in_template, rec, UR_BYTES) << ", " << ur_get(in_template, rec, UR_PACKETS);
-////				cout << ", " << ur_get(in_template, rec, UR_TIME_FIRST) << " - " << ur_get(in_template, rec, UR_TIME_LAST) << ", " << ur_get(in_template, rec, UR_TCP_FLAGS);
-////				cout << "; " << ur_get(in_template, rec, UR_LINK_BIT_FIELD) << ", " << ur_get(in_template, rec, UR_DIR_BIT_FIELD);
-////				cout << "; " << ur_get(in_template, rec, UR_IPV6_TUN_TYPE) << endl;
-//
-//				}
-//				fx.close();
-				continue;
-			}
-		}
+      if ((rec_size < ur_rec_static_size(in_template))) {
+         if (rec_size <= 1) { // end of data
+            break;
+         } else { // data corrupted
+            fprintf(stderr,"Error: Wrong data size. Expected: %i", ur_rec_static_size(in_template));
+            fprintf(stderr," Recieved: %i.\n", rec_size);
+            continue;
+         }
+      }
 
       #pragma omp critical
       {
-			ret = trap_send_data(0, rec, rec_size, TRAP_NO_WAIT);
-//			ret = trap_send_data(0, rec, rec_size, TRAP_WAIT);
-			if (ret != TRAP_E_OK) {
-				if (ret == TRAP_E_TERMINATED) {
-					private_stop = 1; // Module was terminated while waiting for new data (e.g. by Ctrl-C)
-				} else {
-					// Some error has occured
-					fprintf(stderr, "Error: trap_send_data() returned %i (%s)\n", ret, trap_last_error_msg);
-					private_stop = 1;
-				}
-			}
-		} // end critical section
+         ret = trap_send_data(0, rec, rec_size, TRAP_NO_WAIT);
+         if (ret != TRAP_E_OK) {
+            if (ret == TRAP_E_TERMINATED) {
+               private_stop = 1; // Module was terminated while waiting for new data (e.g. by Ctrl-C)
+            } else {
+               // Some error has occured
+               fprintf(stderr, "Error: trap_send_data() returned %i (%s)\n", ret, trap_last_error_msg);
+               private_stop = 1;
+            }
+         }
+      } // end critical section
    } // end while(!stop && !private_stop)
 
-//   if (verbose >= 1) {
+   if (verbose >= 1) {
       printf("Thread %i exitting.\n", index);
-//   }
+   }
 }
 
 int main(int argc, char **argv)
@@ -371,7 +357,7 @@ int main(int argc, char **argv)
    int n_inputs = 0;
 
    // ***** Process parameters *****
-	// Let TRAP library parse command-line arguments and extract its parameters
+   // Let TRAP library parse command-line arguments and extract its parameters
    trap_ifc_spec_t ifc_spec;
    ret = trap_parse_params(&argc, argv, &ifc_spec);
    if (ret != TRAP_E_OK) {
@@ -396,33 +382,33 @@ int main(int argc, char **argv)
          case 'F':
             timestamp_selector = TS_FIRST;
             break;
-			case 'n':
+         case 'n':
             n_inputs = atoi(optarg);
             break;
-			case 'u':
-				out_template_str = optarg;
+         case 'u':
+            out_template_str = optarg;
             in_template_str = optarg;
             break;
-			case 't':
+         case 't':
             initial_timeout = atoi(optarg) * 1000000; // microseconds to seconds
             break;
-			case 'T':
-				mode=MODE_TIME_AWARE;
-				break;
+         case 'T':
+            mode=MODE_TIME_AWARE;
+            break;
          default:
             fprintf(stderr, "Error: Invalid arguments.\n");
             return 1;
       }
    }
 
-	if (n_inputs == 0){
-		fprintf(stderr, "Error: Missing number of input links (parameter -n CNT).\n");
-		ret = -1;
-		goto exit;
-	} else if (n_inputs > 32) {
+   if (n_inputs == 0){
+      fprintf(stderr, "Error: Missing number of input links (parameter -n CNT).\n");
+      ret = -1;
+      goto exit;
+   } else if (n_inputs > 32) {
       fprintf(stderr, "Error: More than 32 interfaces is not allowed by TRAP library.\n");
       ret = -1;
-		goto exit;
+      goto exit;
    }
 
    if (verbose >= 0) {
@@ -432,13 +418,13 @@ int main(int argc, char **argv)
    }
 
    // Create input and output UniRec template
-	in_template = ur_create_template(in_template_str);
-	out_template = ur_create_template(out_template_str);
-	if (in_template == NULL || out_template == NULL) {
-		fprintf(stderr, "Error: Invalid template: %s\n", in_template_str);
-		ret = -2;
-		goto exit;
-	}
+   in_template = ur_create_template(in_template_str);
+   out_template = ur_create_template(out_template_str);
+   if (in_template == NULL || out_template == NULL) {
+      fprintf(stderr, "Error: Invalid template: %s\n", in_template_str);
+      ret = -2;
+      goto exit;
+   }
 
    // ***** TRAP initialization *****
    // Set number of input interfaces
@@ -463,22 +449,22 @@ int main(int argc, char **argv)
    // Register signal handler.
    TRAP_REGISTER_DEFAULT_SIGNAL_HANDLER();
 
-//	trap_ifcctl(TRAPIFC_OUTPUT, 0, TRAPCTL_BUFFERSWITCH, "0");
+// trap_ifcctl(TRAPIFC_OUTPUT, 0, TRAPCTL_BUFFERSWITCH, "0");
 
 
    if (verbose >= 0) {
       printf("Initialization done.\n");
    }
 
-	active_interfaces = n_inputs;
+   active_interfaces = n_inputs;
 
-	 // ***** Start a thread for each interface *****
+    // ***** Start a thread for each interface *****
    #pragma omp parallel num_threads(n_inputs)
    {
-   	if (mode == MODE_TIME_AWARE)
-			ta_capture_thread(omp_get_thread_num());
-		else
-			capture_thread(omp_get_thread_num());
+      if (mode == MODE_TIME_AWARE)
+         ta_capture_thread(omp_get_thread_num());
+      else
+         capture_thread(omp_get_thread_num());
    }
 
    ret = 0;
