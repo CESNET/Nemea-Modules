d699bdea231c8b7f35918cf1c78b86077601808f
Vaclav Bartos <bartos@cesnet.cz>
2016-03-13 23:37:15 +0100

    ipblacklist2idea: Port field removed from IDEA
    - it turned out that Ports might not be valid, since ipblacklistfilter aggregates flows by IP&protocol only.

--- report2idea/ipblacklist2idea.py
+++ report2idea/ipblacklist2idea.py
@@ -15,7 +15,7 @@ MODULE_NAME = "ipblacklist2idea"
 MODULE_DESC = "Converts output of ipblacklistfilter module to IDEA."
 
 REQ_TYPE = trap.TRAP_FMT_UNIREC
-REQ_FORMAT = "ipaddr DST_IP,ipaddr SRC_IP,uint64 BYTES,uint64 DST_BLACKLIST,uint64 SRC_BLACKLIST,time TIME_FIRST,time TIME_LAST,uint32 EVENT_SCALE,uint32 PACKETS,uint16 DST_PORT,uint16 SRC_PORT"
+REQ_FORMAT = "ipaddr DST_IP,ipaddr SRC_IP,uint64 BYTES,uint64 DST_BLACKLIST,uint64 SRC_BLACKLIST,time TIME_FIRST,time TIME_LAST,uint32 EVENT_SCALE,uint32 PACKETS"
 
 
 
@@ -112,8 +112,7 @@ def convert_to_idea(rec, opts=None):
 
     if rec.DST_IP != 0:
         addr = {
-            "Proto": [ protocol ],
-            "Port": [ rec.DST_PORT ]
+            "Proto": [ protocol ]
         }
         setAddr(addr, rec.DST_IP)
 
@@ -132,8 +131,7 @@ def convert_to_idea(rec, opts=None):
 
     if rec.SRC_IP != 0:
         addr = {
-            "Proto": [ protocol ],
-            "Port": [ rec.SRC_PORT ]
+            "Proto": [ protocol ]
         }
         setAddr(addr, rec.SRC_IP)
 
