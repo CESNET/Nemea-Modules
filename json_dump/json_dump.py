80d1bace61d92c8c9b53c57e4c67ced3e8bb05bb
Tomas Cejka <cejkat@cesnet.cz>
2016-07-02 22:52:21 +0200

    json_{dump,replay}: adaptation of pytrap

--- json_dump/json_dump.py
+++ json_dump/json_dump.py
@@ -2,13 +2,14 @@
 
 import sys
 import os.path
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "nemea-framework", "python"))
-import trap
+import pytrap
 import json
 import optparse # TODO change TRAP Python wrapper to use argparse
 
 from optparse import OptionParser
 parser = OptionParser(add_help_option=False)
+parser.add_option("-i", "--ifcspec", dest="ifcspec",
+      help="TRAP IFC specifier", metavar="IFCSPEC")
 parser.add_option("-w", dest="filename",
     help="Write dump to FILE instead of stdout (overwrite file)", metavar="FILE")
 parser.add_option("-a", dest="filename_append",
@@ -16,21 +17,12 @@ parser.add_option("-a", dest="filename_append",
 parser.add_option("-I", "--indent", metavar="N", type=int,
     help="Pretty-print JSON with indentation set to N spaces. Note that such format can't be read by json_replay module.")
 
-module_info = trap.CreateModuleInfo(
-    "JSON dump", # Module name
-    "Dump JSON data from input TRAP interface to file or standard output. Each JSON record is written to separate line.", # Description
-    1, # Number of input interfaces
-    0  # Number of output interfaces
-)
-
-
-# Initialize module
-ifc_spec = trap.parseParams(sys.argv, module_info)
-trap.init(module_info, ifc_spec)
-trap.registerDefaultSignalHandler()
-
 # Parse remaining command-line arguments
 (options, args) = parser.parse_args()
+
+# Initialize module
+trap = pytrap.TrapCtx()
+trap.init(["-i", options.ifcspec])
 
 # Open output file
 if options.filename and options.filename_append:
@@ -44,33 +36,34 @@ else:
     file = sys.stdout
 
 # Set JSON as required data type on input
-trap.set_required_fmt(0, trap.TRAP_FMT_JSON, "")
+trap.setRequiredFmt(0, pytrap.FMT_JSON, "")
 
+stop = False
 # Main loop (trap.stop is set to True when SIGINT or SIGTERM is received)
-while not trap.stop:
+while not stop:
     # Read data from input interface
     try:
-        data = trap.recv(0)
-    except trap.EFMTMismatch:
+        data = trap.recv()
+    except pytrap.FormatMismatch:
         sys.stderr.write("Error: output and input interfaces data type or format mismatch\n")
         break
-    except trap.EFMTChanged as e:
+    except pytrap.FormatChanged as e:
         # TODO: if verbose print message about new format
         data = e.data
+        del(e)
         pass
-    except trap.ETerminated:
+    except pytrap.Terminated:
         break
 
     # Check for "end-of-stream" record
     if len(data) <= 1:
         break
 
-    # Decode data (and check it's valid JSON)
-    rec = json.loads(data)
-    # TODO catch exception if it's not valid JSON - print error message to stderr and continue
-
-    # neprehazi se poradi polozek? mozna by bylo lepsi vypisovat primo "data" (pokud neni zapnuto indent)
-
-    # Print it to file or stdout
-    file.write(json.dumps(rec, indent=options.indent) + '\n')
+    try:
+        # Decode data (and check it's valid JSON)
+        rec = json.loads(data.decode("utf-8"))
+        # Print it to file or stdout
+        file.write(json.dumps(rec, indent=options.indent) + '\n')
+    except ValueException:
+        pass
 
--- json_replay/json_replay.py
+++ json_replay/json_replay.py
@@ -2,36 +2,27 @@
 
 import sys
 import os.path
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "nemea-framework", "python"))
-import trap
+import pytrap
 import json
 import optparse # TODO change TRAP Python wrapper to use argparse
 
 from optparse import OptionParser
 parser = OptionParser(add_help_option=False)
+parser.add_option("-i", "--ifcspec", dest="ifcspec",
+      help="TRAP IFC specifier", metavar="IFCSPEC")
 parser.add_option("-f", "--format", dest="format", default="",
     help="Set format identifier string (this must match the format ID required by receiving module)", metavar="FMT_ID")
 parser.add_option("-n", "--no-eos", action="store_true",
     help="Don't send end-of-stream message at the end.")
-
-
-module_info = trap.CreateModuleInfo(
-    "JSON replay", # Module name
-    "Replay JSON data read from input file or stdin to TRAP interface. "
-    "Each line of input must contain exactly one JSON record.",
-    0, # Number of input interfaces
-    1,  # Number of output interfaces
-    parser
-)
-
-
-# Initialize module
-ifc_spec = trap.parseParams(sys.argv, module_info)
-trap.init(module_info, ifc_spec)
-trap.registerDefaultSignalHandler()
+parser.add_option("-v", "--verbose", action="store_true",
+    help="Set verbose mode - print messages.")
 
 # Parse remaining command-line arguments
 (options, args) = parser.parse_args()
+
+# Initialize module
+trap = pytrap.TrapCtx()
+trap.init(sys.argv, 0, 1)
 
 if len(args) > 1:
     sys.stderr.write("Error: Only one input file can be specified.")
@@ -41,21 +32,22 @@ else:
     file = sys.stdin
 
 # Set format of data on the output interface
-trap.set_data_fmt(0, trap.TRAP_FMT_JSON, options.format)
+trap.setDataFmt(0, pytrap.FMT_JSON, options.format)
 
 # Set timeout of output interface to "WAIT"
-trap.ifcctl(trap.IFC_OUTPUT, 0, trap.CTL_SETTIMEOUT, trap.WAIT)
+trap.ifcctl(0, False, pytrap.CTL_TIMEOUT, pytrap.TIMEOUT_WAIT)
 
 # Main loop (trap.stop is set to True when SIGINT or SIGTERM is received)
 i = 0 # line index
-while not trap.stop:
+stop = False
+while not stop:
     # Read one record from input file
     line = sys.stdin.readline()
     i += 1
     if not line: # EOF
         # Send end-of-stream message and exit
         if not options.no_eos:
-            trap.send(0, "0")
+            trap.send(bytearray(b"0"))
         trap.sendFlush(0)
         break
     if line == "\n":
@@ -68,7 +60,9 @@ while not trap.stop:
         sys.stderr.write("Error: Can't decode JSON record on line "+str(i)+". The line is skipped.\n")
         continue
 
+    if options.verbose:
+        print(json.dumps(rec))
+
     # Send JSON to output ifc
-    #print "Sending", json.dumps(rec) # TODO verbose mode which would print what is being sent
-    trap.send(0, json.dumps(rec))
+    trap.send(bytearray(json.dumps(rec), "utf-8"))
 
