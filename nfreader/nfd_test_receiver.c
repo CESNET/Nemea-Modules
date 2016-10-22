02c50f8c6da3da1701c30757e9fe7a066cf53b75
Jiri Havranek <havraji6@fit.cvut.cz>
2016-10-22 12:14:44 +0200

    nfreader: code style

--- nfreader/nfd_test_receiver.c
+++ nfreader/nfd_test_receiver.c
@@ -51,8 +51,11 @@
 #include <libtrap/trap.h>
 #include <unirec/unirec.h>
 #include "fields.c"
-// Struct with information about module
+
+/* Struct with information about module. */
 trap_module_info_t *module_info = NULL;
+
+#define BASIC_FLOW_TEMPLATE "SRC_IP,DST_IP,SRC_PORT,DST_PORT,PROTOCOL,PACKETS,BYTES,TIME_FIRST,TIME_LAST,TCP_FLAGS,LINK_BIT_FIELD,DIR_BIT_FIELD,TOS,TTL"
 
 UR_FIELDS (
    ipaddr DST_IP,
@@ -88,7 +91,7 @@ UR_FIELDS (
 
 static int stop = 0;
 
-// Function to handle SIGTERM and SIGINT signals (used to stop the module)
+/* Function to handle SIGTERM and SIGINT signals (used to stop the module). */
 TRAP_DEFAULT_SIGNAL_HANDLER(stop = 1);
 
 int main(int argc, char **argv)
@@ -106,12 +109,12 @@ int main(int argc, char **argv)
    uint64_t msg_counter = 0;
    uint64_t act_msg_counter = 0;
 
-   INIT_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+   INIT_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS);
 
-   // Let TRAP library parse command-line arguments and extract its parameters
+   /* Let TRAP library parse command-line arguments and extract its parameters. */
    ret = trap_parse_params(&argc, argv, &ifc_spec);
    if (ret != TRAP_E_OK) {
-      if (ret == TRAP_E_HELP) { // "-h" was found
+      if (ret == TRAP_E_HELP) { /* "-h" was found. */
          trap_print_help(&module_info);
          return 0;
       }
@@ -119,7 +122,7 @@ int main(int argc, char **argv)
       return 1;
    }
 
-   // Parse remaining parameters
+   /* Parse remaining parameters. */
    signed char opt;
    while ((opt = TRAP_GETOPT(argc, argv, module_getopt_string, long_options)) != -1) {
       switch (opt) {
@@ -130,7 +133,7 @@ int main(int argc, char **argv)
             time_interval = atoi(optarg);
             if (time_interval == 0) {
                fprintf(stderr, "Invalid time interval (-t).\n");
-               FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+               FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS);
                return 2;
             }
          case 'F':
@@ -138,54 +141,54 @@ int main(int argc, char **argv)
             break;
          default:
             fprintf(stderr, "Invalid arguments.\n");
-            FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+            FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS);
             return 2;
       }
    }
 
    if (optind > argc) {
       fprintf(stderr, "Wrong number of parameters.\nUsage: %s -i trap-ifc-specifier [-t N]\n", argv[0]);
-      FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+      FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS);
       return 2;
    }
 
    if (differences && fields) {
       fprintf(stderr, "Wrong parameters, use only one of \"-F\" or \"-D\"\n.");
-      FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+      FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS);
       return 2;
    }
 
-   // Initialize TRAP library (create and init all interfaces)
+   /* Initialize TRAP library (create and init all interfaces). */
    ret = trap_init(&module_info, ifc_spec);
    if (ret != TRAP_E_OK) {
       fprintf(stderr, "ERROR in TRAP initialization: %s\n", trap_last_error_msg);
-      FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+      FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS);
       return 4;
    }
 
-   trap_free_ifc_spec(ifc_spec); // We don't need ifc_spec anymore
+   trap_free_ifc_spec(ifc_spec); /* We don't need ifc_spec anymore. */
 
 
 
-   // ***** Create UniRec templates *****
-   ur_template_t *in_tmplt = ur_create_input_template(0, "SRC_IP,DST_IP,SRC_PORT,DST_PORT,PROTOCOL,PACKETS,BYTES,TIME_FIRST,TIME_LAST,TCP_FLAGS,LINK_BIT_FIELD,DIR_BIT_FIELD,TOS,TTL", NULL);
+   /* Create UniRec templates. */
+   ur_template_t *in_tmplt = ur_create_input_template(0, BASIC_FLOW_TEMPLATE, NULL);
 
-   // ***** Main processing loop *****
+   /* Main processing loop. */
 
-   // Read data from input, process them and write to output
+   /* Read data from input, process them and write to output. */
    while (!stop) {
       const void *in_rec;
       uint16_t in_rec_size;
 
-      // Receive data from any input interface, wait until data are available
+      /* Receive data from any input interface, wait until data are available. */
       ret = TRAP_RECEIVE(0, in_rec, in_rec_size, in_tmplt);
-      // Handle possible errors
+      /* Handle possible errors. */
       TRAP_DEFAULT_GET_DATA_ERROR_HANDLING(ret, continue, break);
 
-      // Check size of received data
+      /* Check size of received data. */
       if (in_rec_size < ur_rec_fixlen_size(in_tmplt)) {
          if (in_rec_size <= 1) {
-            break; // End of data (used for testing purposes)
+            break; /* End of data (used for testing purposes). */
          } else {
             fprintf(stderr, "Error: data with wrong size received (expected size: >= %hu, received size: %hu)\n",
                     ur_rec_fixlen_size(in_tmplt), in_rec_size);
@@ -193,7 +196,7 @@ int main(int argc, char **argv)
          }
       }
 
-      // PROCESS THE DATA
+      /* Process the data. */
       ++msg_counter;
       first = ur_time_get_sec(ur_get(in_tmplt, in_rec, F_TIME_FIRST));
       last = ur_time_get_sec(ur_get(in_tmplt, in_rec, F_TIME_LAST));
@@ -220,17 +223,17 @@ int main(int argc, char **argv)
 
    }
 
-   // ***** Cleanup *****
+   /* Cleanup. */
    if (time_interval) {
       printf("Time interval %lu - %lu: %llu messages.\n", act_time, next_time, msg_counter - act_msg_counter);
    }
    fprintf(stderr, "TOTAL RECEIVED MESSAGES: %llu\n", msg_counter);
 
-   // Do all necessary cleanup before exiting
+   /* Do all necessary cleanup before exiting. */
    TRAP_DEFAULT_FINALIZATION();
 
    ur_free_template(in_tmplt);
-   FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+   FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS);
 
    return 0;
 }
