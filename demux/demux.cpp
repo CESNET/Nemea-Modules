fb07bc22734a343457ad7a5f60028f6d39a2d351
Tomas Cejka <cejkat@cesnet.cz>
2018-02-02 11:52:22 +0100

    mux: changed code according to coding style
    Co-authored-by: Dominik Soukup <soukudom@cesnet.cz>

--- demux/demux.cpp
+++ demux/demux.cpp
@@ -75,157 +75,155 @@ int verbose = 0;
 trap_module_info_t *module_info = NULL;
 
 #define MODULE_BASIC_INFO(BASIC) \
-  BASIC("demux","This module split united input to more outputs",1,-1)
+  BASIC("demux", "This module split united input to more outputs", 1, -1)
 #define MODULE_PARAMS(PARAM) \
 PARAM('n', "link_count", "Sets count of output links. Must correspond to parameter -i (trap).", required_argument, "int32")
 
 
-int main (int argc, char ** argv){
-    //int ret = 0;
-    INIT_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
-    trap_ifc_spec_t ifc_spec;
-    ret = trap_parse_params(&argc, argv, &ifc_spec);
-    if (ret != TRAP_E_OK) {
-        if (ret == TRAP_E_HELP) { // "-h" was found
-        trap_print_help(module_info);
-        return 0;
-        }
-        ///fprintf(stderr, "ERROR in parsing of parameters for TRAP: %s\n", trap_last_error_msg);
-        cerr << "ERROR in parsing of parameters for TRAP: " << trap_last_error_msg << endl;
-        return 1;
-    }   
+int main (int argc, char ** argv)
+{
+   //int ret = 0;
+   INIT_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+   trap_ifc_spec_t ifc_spec;
+   ret = trap_parse_params(&argc, argv, &ifc_spec);
+   if (ret != TRAP_E_OK) {
+      if (ret == TRAP_E_HELP) { // "-h" was found
+         trap_print_help(module_info);
+         return 0;
+      }
+      ///fprintf(stderr, "ERROR in parsing of parameters for TRAP: %s\n", trap_last_error_msg);
+      cerr << "ERROR in parsing of parameters for TRAP: " << trap_last_error_msg << endl;
+      return 1;
+   }
 
-    // Parse remaining parameters and get configuration
-    signed char opt;
-    while ((opt = TRAP_GETOPT(argc, argv, module_getopt_string, long_options)) != -1) {
-        switch (opt) {
-        case 'n':
-            n_outputs = atoi(optarg);
-            break;
-        default:
-            //fprintf(stderr, "Error: Invalid arguments.\n");
-            cerr <<  "Error: Invalid arguments." << endl;
-            FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
-            return 1;
-        }
-    }
+   // Parse remaining parameters and get configuration
+   signed char opt;
+   while ((opt = TRAP_GETOPT(argc, argv, module_getopt_string, long_options)) != -1) {
+      switch (opt) {
+      case 'n':
+         n_outputs = atoi(optarg);
+         break;
+      default:
+         //fprintf(stderr, "Error: Invalid arguments.\n");
+         cerr <<  "Error: Invalid arguments." << endl;
+         FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+         return 1;
+      }
+   }
 
-    verbose = trap_get_verbose_level();
-    if (verbose >= 0) {
-        //printf("Verbosity level: %i\n", trap_get_verbose_level());
-        cout << "Verbosity level: " <<  trap_get_verbose_level();
-    }   
+   verbose = trap_get_verbose_level();
+   if (verbose >= 0) {
+      //printf("Verbosity level: %i\n", trap_get_verbose_level());
+      cout << "Verbosity level: " <<  trap_get_verbose_level();
+   }
 
-    if (verbose >= 0) {
-        //printf("Number of outputs: %i\n", n_outputs);
-        cout << "Number of outputs: " << n_outputs << endl;
-    }   
-    if (n_outputs < 1) {
-        //fprintf(stderr, "Error: Number of input interfaces must be positive integer.\n");
-        cerr << "Error: Number of output interfaces must be positive integer." << endl;
-        FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
-        return 0;
-    }   
-    if (n_outputs > 32) {
-        //fprintf(stderr, "Error: More than 32 interfaces is not allowed by TRAP library.\n");
-        cerr << "Error: More than 32 interfaces is not allowed by TRAP library." << endl;
-        FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
-        return 4;
-    }   
+   if (verbose >= 0) {
+      //printf("Number of outputs: %i\n", n_outputs);
+      cout << "Number of outputs: " << n_outputs << endl;
+   }
+   if (n_outputs < 1) {
+      //fprintf(stderr, "Error: Number of input interfaces must be positive integer.\n");
+      cerr << "Error: Number of output interfaces must be positive integer." << endl;
+      FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+         return 0;
+   }
+   if (n_outputs > 32) {
+      //fprintf(stderr, "Error: More than 32 interfaces is not allowed by TRAP library.\n");
+      cerr << "Error: More than 32 interfaces is not allowed by TRAP library." << endl;
+      FREE_MODULE_INFO_STRUCT(MODULE_BASIC_INFO, MODULE_PARAMS)
+         return 4;
+   }
 
-    // Set number of output interfaces
-    module_info->num_ifc_out = n_outputs;
+   // Set number of output interfaces
+   module_info->num_ifc_out = n_outputs;
 
-    if (verbose >= 0) {
-        cout << "Initializing TRAP library ..." << endl;
-    }   
+   if (verbose >= 0) {
+      cout << "Initializing TRAP library ..." << endl;
+   }
 
-    ctx = trap_ctx_init(module_info, ifc_spec);
+   ctx = trap_ctx_init(module_info, ifc_spec);
 
-    if (ctx == NULL){
-        cerr << "ERROR in TRAP initialization: " << trap_last_error_msg << endl;
-        exit_value=1;
-        goto cleanup;
-    }
+   if (ctx == NULL) {
+      cerr << "ERROR in TRAP initialization: " << trap_last_error_msg << endl;
+      exit_value=1;
+      goto cleanup;
+   }
 
-    //check if number of interfaces is correct
-    if (strlen(ifc_spec.types) <= 1){
-        cerr << "ERROR expected at least 1 input and 1 output interface. Got only 1." << endl;
-        return 1;
-    }
+   //check if number of interfaces is correct
+   if (strlen(ifc_spec.types) <= 1) {
+      cerr << "ERROR expected at least 1 input and 1 output interface. Got only 1." << endl;
+      return 1;
+   }
 
-    //check if number of output interfaces is correct
-    if (strlen(ifc_spec.types)-1 != n_outputs){
-        cerr << "ERROR number of output interfaces is incorrect" << endl;
-        goto cleanup;
-    }
+   //check if number of output interfaces is correct
+   if (strlen(ifc_spec.types) -1 != n_outputs) {
+      cerr << "ERROR number of output interfaces is incorrect" << endl;
+      goto cleanup;
+   }
 
-    //input interface control settings
-    if (trap_ctx_ifcctl(ctx, TRAPIFC_INPUT,0,TRAPCTL_SETTIMEOUT,TRAP_WAIT) != TRAP_E_OK){
-        cerr << "ERROR in input interface initialization" << endl;
-        exit_value = 2;
-        goto cleanup;
-    }
+   //input interface control settings
+   if (trap_ctx_ifcctl(ctx, TRAPIFC_INPUT, 0, TRAPCTL_SETTIMEOUT, TRAP_WAIT) != TRAP_E_OK) {
+      cerr << "ERROR in input interface initialization" << endl;
+      exit_value = 2;
+      goto cleanup;
+   }
 
-    //allocate memory for output templates
-    out_templates = (ur_template_t **)calloc(n_outputs,sizeof(*out_templates));
-    if (out_templates == NULL){
-        cerr <<  "Memory allocation error." << endl;
-        goto cleanup;
-    }
+   //allocate memory for output templates
+   out_templates = (ur_template_t **) calloc(n_outputs, sizeof(*out_templates));
+   if (out_templates == NULL) {
+      cerr <<  "Memory allocation error." << endl;
+      goto cleanup;
+   }
 
-    //initialize and set templates for UniRec negotiation
-    for (int i = 0; i < n_outputs; i++){
-        //output interfaces control settings
-        if (trap_ctx_ifcctl(ctx, TRAPIFC_OUTPUT,i,TRAPCTL_SETTIMEOUT,TRAP_NO_WAIT) != TRAP_E_OK){
-            cerr << "ERROR in output interface initialization" << endl;
-            exit_value = 2;
-            goto cleanup;
-        }
-        //create empty output template
-        out_templates[i] = ur_ctx_create_output_template(ctx,i,NULL,NULL);
-        if (out_templates[i] == NULL){
-            cerr << "Memory allocation error." << endl;
-            goto cleanup;
-        }
-        //set format for output interfaces
-        trap_ctx_set_data_fmt(ctx, i, TRAP_FMT_UNIREC, NULL);
-    }
+   //initialize and set templates for UniRec negotiation
+   for (int i = 0; i < n_outputs; i++) {
+      //output interfaces control settings
+      if (trap_ctx_ifcctl(ctx, TRAPIFC_OUTPUT, i, TRAPCTL_SETTIMEOUT, TRAP_NO_WAIT) != TRAP_E_OK) {
+         cerr << "ERROR in output interface initialization" << endl;
+         exit_value = 2;
+         goto cleanup;
+      }
+      //create empty output template
+      out_templates[i] = ur_ctx_create_output_template(ctx, i, NULL, NULL);
+      if (out_templates[i] == NULL) {
+         cerr << "Memory allocation error." << endl;
+         goto cleanup;
+      }
+      //set format for output interfaces
+      trap_ctx_set_data_fmt(ctx, i, TRAP_FMT_UNIREC, NULL);
+   }
 
-    //set required incoming format
-    trap_ctx_set_required_fmt(ctx,0,TRAP_FMT_RAW);
+   //set required incoming format
+   trap_ctx_set_required_fmt(ctx, 0, TRAP_FMT_RAW);
 
-    //main loop
-    while (!stop){
-        ret = trap_ctx_recv(ctx,0, &data_nemea_output, &memory_received);
-        //process received data
-        meta_info_t *ptr = (meta_info_t *) data_nemea_output;
-        
-        //set new template
-        if (ptr->messageID == 2){
-            if (verbose >= 0){
-                cout << "Hello message has been received. Setting data format for the output interface with index " << ptr->interfaceID << endl;
-            }
-            trap_ctx_set_data_fmt(ctx,ptr->interfaceID,ptr->data_fmt,(((char *)(data_nemea_output)+5)) );
-        }
+   //main loop
+   while (!stop) {
+      ret = trap_ctx_recv(ctx, 0, &data_nemea_output, &memory_received);
+      //process received data
+      meta_info_t *ptr = (meta_info_t *) data_nemea_output;
 
-        //forward data to appropriate interface
-        else if (ptr->messageID == 1){
-            if (verbose >= 0){
-                cout << "Normal data message has been received. Forwarding to the output interface with index " << ptr->interfaceID << endl;
-            }
-            //send only payload data
-            ret = trap_ctx_send(ctx,ptr->interfaceID,ptr->payload,memory_received-HEADER_SIZE);
-        }
-        else {
-            cerr << "ERROR: Received message is not valid!" << endl;
-        }
-    }
-
-
+      //set new template
+      if (ptr->messageID == 2) {
+         if (verbose >= 0) {
+            cout << "Hello message has been received. Setting data format for the output interface with index " << ptr->interfaceID << endl;
+         }
+         trap_ctx_set_data_fmt(ctx, ptr->interfaceID, ptr->data_fmt, (((char *) (data_nemea_output) + 5)));
+      } else if (ptr->messageID == 1) {
+         //forward data to appropriate interface
+         if (verbose >= 0) {
+            cout << "Normal data message has been received. Forwarding to the output interface with index " << ptr->interfaceID << endl;
+         }
+         //send only payload data
+         ret = trap_ctx_send(ctx, ptr->interfaceID, ptr->payload, memory_received-HEADER_SIZE);
+      }
+      else {
+         cerr << "ERROR: Received message is not valid!" << endl;
+      }
+   }
 
 cleanup:
-    //cleaning
-    trap_ctx_finalize(&ctx);
-    return 0;
+   //cleaning
+   trap_ctx_finalize(&ctx);
+   return 0;
 }
+
