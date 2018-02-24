0ee6bb8513920ff7a2612002ff07bf8b4d31012d
Michal Slabihoudek <slabimic@fit.cvut.cz>
2018-02-24 19:42:01 +0100

    Sum and Avg functions created and implemented

---
+++ aggregator/agg_functions.h
@@ -0,0 +1,33 @@
+//
+// Created by slabimic on 24/02/18.
+//
+
+#include <unirec/unirec.h>
+
+#ifndef AGGREGATOR_AGG_FUNCTIONS_H
+#define AGGREGATOR_AGG_FUNCTIONS_H
+
+#endif //AGGREGATOR_AGG_FUNCTIONS_H
+
+
+void sum_int64(const void *src, void *dst);
+void sum_int32(const void *src, void *dst);
+void sum_int16(const void *src, void *dst);
+void sum_int8(const void *src, void *dst);
+void sum_uint64(const void *src, void *dst);
+void sum_uint32(const void *src, void *dst);
+void sum_uint16(const void *src, void *dst);
+void sum_uint8(const void *src, void *dst);
+void sum_float(const void *src, void *dst);
+void sum_double(const void *src, void *dst);
+
+void avg_int64(const void *src, void *dst);
+void avg_int32(const void *src, void *dst);
+void avg_int16(const void *src, void *dst);
+void avg_int8(const void *src, void *dst);
+void avg_uint64(const void *src, void *dst);
+void avg_uint32(const void *src, void *dst);
+void avg_uint16(const void *src, void *dst);
+void avg_uint8(const void *src, void *dst);
+void avg_float(const void *src, void *dst);
+void avg_double(const void *src, void *dst);
