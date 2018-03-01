cc3254b99e551754fc2632719d4ef29210064779
Michal Slabihoudek <slabimic@fit.cvut.cz>
2018-03-01 16:19:08 +0100

    size_of_fields[] attribute removed, no longer needed by module

--- aggregator/output.cpp
+++ aggregator/output.cpp
@@ -14,18 +14,19 @@
 /*
  * Static variables declaration, better than global variable
  */
+ur_template_t *OutputTemplate::out_tmplt = NULL;
 int OutputTemplate::indexes_to_record [MAX_KEY_FIELDS];
-int OutputTemplate::sizes_of_fields [MAX_KEY_FIELDS];
+//int OutputTemplate::sizes_of_fields [MAX_KEY_FIELDS];
 agg_func (OutputTemplate::process[MAX_KEY_FIELDS]);
 int OutputTemplate::used_fields = 0;
 bool OutputTemplate::prepare_to_send = false;
 bool OutputTemplate::avg_fields[MAX_KEY_FIELDS];
 
 /* ----------------------------------------------------------------- */
-void OutputTemplate::add_field(int record_id, int size, agg_func foo, bool avg)
+void OutputTemplate::add_field(int record_id, agg_func foo, bool avg)
 {
    indexes_to_record[used_fields] = record_id;
-   sizes_of_fields[used_fields] = size;
+   //sizes_of_fields[used_fields] = size;
    process[used_fields] = foo;
    avg_fields[used_fields] = avg;
    // If avg used for the first time set prepare_to_send flag
--- aggregator/output.h
+++ aggregator/output.h
@@ -13,16 +13,16 @@ typedef void (*agg_func)(const void *src, void *dst);    // Define pointer to ag
 
 class OutputTemplate {
 public:
-   ur_template_t *out_tmplt;
+   static ur_template_t *out_tmplt;
    static int indexes_to_record[MAX_KEY_FIELDS];
-   static int sizes_of_fields[MAX_KEY_FIELDS];
+   //static int sizes_of_fields[MAX_KEY_FIELDS];
    static int used_fields;
    //static void (*agg_func[MAX_KEY_FIELDS])(const void *src, void *dst);
    static agg_func process[MAX_KEY_FIELDS];
    static bool prepare_to_send;
    static bool avg_fields[MAX_KEY_FIELDS];
 
-   static void add_field(int record_id, int size, agg_func foo, bool avg_flag);
+   static void add_field(int record_id, agg_func foo, bool avg_flag);
 };
 
 #endif //AGGREGATOR_OUTPUT_H
