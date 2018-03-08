1900c406590fbdd54882cc2a2c189cb93d15a28a
Michal Slabihoudek <slabimic@fit.cvut.cz>
2018-03-08 22:38:26 +0100

    New template function make_avg added and implemented

--- aggregator/agg_functions.h
+++ aggregator/agg_functions.h
@@ -7,8 +7,6 @@
 #ifndef AGGREGATOR_AGG_FUNCTIONS_H
 #define AGGREGATOR_AGG_FUNCTIONS_H
 
-#endif //AGGREGATOR_AGG_FUNCTIONS_H
-
 
 void sum_int64(const void *src, void *dst);
 void sum_int32(const void *src, void *dst);
@@ -31,6 +29,21 @@ void avg_uint16(const void *src, void *dst);
 void avg_uint8(const void *src, void *dst);
 void avg_float(const void *src, void *dst);
 void avg_double(const void *src, void *dst);
-
+/*
+template <typename T>
+void make_avg(void *src, uint32_t count);
+*/
+/*
+ * Implementation in header file because of errors
+ * aggregation_module-configuration.o: undefined reference to `void make_avg<unsigned char>(void*, unsigned int)'
+*/
+template <typename T>
+void make_avg(void *src, uint32_t count)
+{
+   *((T*)src) /= count;
+}
 
 void nope(const void *src, void *dst);
+
+
+#endif //AGGREGATOR_AGG_FUNCTIONS_H
