af365eed8b19b922fa64f27a8ea3cb8fc4783cad
Michal Slabihoudek <slabimic@fit.cvut.cz>
2018-03-16 21:50:27 +0100

    Sum and avg functions modified to templates.

--- aggregator/agg_functions.h
+++ aggregator/agg_functions.h
@@ -8,27 +8,17 @@
 #define AGGREGATOR_AGG_FUNCTIONS_H
 
 
-void sum_int64(const void *src, void *dst);
-void sum_int32(const void *src, void *dst);
-void sum_int16(const void *src, void *dst);
-void sum_int8(const void *src, void *dst);
-void sum_uint64(const void *src, void *dst);
-void sum_uint32(const void *src, void *dst);
-void sum_uint16(const void *src, void *dst);
-void sum_uint8(const void *src, void *dst);
-void sum_float(const void *src, void *dst);
-void sum_double(const void *src, void *dst);
+template <typename T>
+void sum(const void *src, void *dst)
+{
+   *((T*)dst) += *((T*)src);
+}
 
-void avg_int64(const void *src, void *dst);
-void avg_int32(const void *src, void *dst);
-void avg_int16(const void *src, void *dst);
-void avg_int8(const void *src, void *dst);
-void avg_uint64(const void *src, void *dst);
-void avg_uint32(const void *src, void *dst);
-void avg_uint16(const void *src, void *dst);
-void avg_uint8(const void *src, void *dst);
-void avg_float(const void *src, void *dst);
-void avg_double(const void *src, void *dst);
+template <typename T>
+void avg(const void *src, void *dst)
+{
+   sum<T>(src, dst);
+}
 
 /*
  * Implementation in header file because of errors
