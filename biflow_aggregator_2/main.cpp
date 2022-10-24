5b52496b6f050fbe49e1af57f678eaaadbe9b064
xsiska12 <xsiska12@stud.fit.vutbr.cz>
2022-10-24 19:16:55 +0000

    biflow-aggregator - update main.cpp

--- biflow_aggregator_2/main.cpp
+++ biflow_aggregator_2/main.cpp
@@ -1,3 +1,79 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @brief Bidirectional flow (bi-flow) aggregator 
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <cstdlib>
+#include <memory>
+#include <iostream>
+#include <stdexcept>
+
+#include "aggregatorConfigParser.hpp"
+#include "aggregatorConfigParserFactory.hpp"
+
+#include "config.hpp"
+#include "libtrapAdapter.hpp"
+#include "unirecAdapter.hpp"
+
+#include "flowKeyUnirecDescription.hpp"
+
+using namespace aggregator;
+
+/**
+ * @brief Build TrapModuleInfo class
+ */
+TrapModuleInfo BuildTrapModuleInfo()
+{
+	TrapModuleInfo trapModuleInfo;
+	trapModuleInfo.SetModuleName("aggregator");
+	trapModuleInfo.SetModuleDescription("TODO WiP");
+	trapModuleInfo.SetIfcCount(1, 1);
+	return trapModuleInfo;
+}
+
 int main(int argc, char** argv)
 {
+	Config config;
+	TrapModuleInfo trapModuleInfo = BuildTrapModuleInfo();
+
+	Libtrap libtrap(trapModuleInfo.GetModuleInfo());
+	Unirec unirec;
+
+	try {
+		libtrap.Init(argc, argv);
+		const char* requiredInputTemplate = "TIME_FIRST,TIME_LAST";
+		unirec.CreateInputTemplate(requiredInputTemplate);
+	} catch (const std::exception& ex) {
+		std::cerr << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try {
+		config.Parse(argc, argv);
+	} catch (const std::invalid_argument& ex) {
+		std::cerr << ex.what() << std::endl;
+		config.PrintUsage();
+		return EXIT_FAILURE;
+	}
+
+	if (config.IsHelp()) {
+		config.PrintUsage();
+		return EXIT_SUCCESS;
+	}
+
+	std::unique_ptr<AggConfigParser> configParser;
+
+	try {
+		configParser = AggConfigParserFactory::instance().Create(
+			config.GetConfigParserSpecification());
+		configParser->Parse();
+	} catch (const std::exception& ex) {
+		std::cerr << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
