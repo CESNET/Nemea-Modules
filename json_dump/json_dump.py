d7f2db870e21c6bd53447c88d4c355a249554004
Václav Bartoš <bartos@cesnet.cz>
2023-03-31 22:35:54 +0200

    json_dump reconnection improvements
    Incorporated a fix from PR220 and suggestions by jaroslavpesek, and made a few other improvements.

--- json_dump/json_dump.py
+++ json_dump/json_dump.py
@@ -6,7 +6,7 @@ import json
 import socket
 import sys
 import time
-from typing import Optional
+from typing import Optional, TextIO
 
 import pytrap
 
@@ -17,7 +17,7 @@ def get_parser() -> argparse.ArgumentParser:
     Returns:
          An instance of ArgumentParser with ready arguments.
     """
-    parser = argparse.ArgumentParser(add_help=True)
+    parser = argparse.ArgumentParser(description="Print received JSON messages to stdout or a file.")
     parser.add_argument("-i", "--ifcspec", dest="ifcspec", metavar="IFCSPEC",
                         required=True,
                         help="See https://nemea.liberouter.org/trap-ifcspec/")
@@ -30,17 +30,17 @@ def get_parser() -> argparse.ArgumentParser:
                         help="Disable automatic flush of output buffer after writing a "
                              "record (may improve performance).")
 
-    group = parser.add_mutually_exclusive_group(required=True)
+    group = parser.add_mutually_exclusive_group()
     group.add_argument("-w", dest="filename", metavar="FILE",
-                       help="Write dump to FILE instead of stdout (overwrite file)")
+                       help="Write data to FILE instead of stdout (overwrite file)")
     group.add_argument("-a", dest="filename_append", metavar="FILE",
-                       help="Write dump to FILE instead of stdout (append to file)")
+                       help="Write data to FILE instead of stdout (append to file)")
     group.add_argument("-s", dest="networktarget", metavar="HOST:PORT",
-                       help="Write dump using a TCP network stream to HOST:PORT")
+                       help="Send data using a TCP network stream to HOST:PORT")
     return parser
 
 
-def connect_socket(address: str, port: int) -> socket.SocketIO:
+def connect_socket(address: str, port: int, wait_interval: int = 5) -> TextIO:
     """Create a connection to a socket given an address and a port.
 
     The connection is tried repeatedly until successful. If the connection to the socket
@@ -49,23 +49,26 @@ def connect_socket(address: str, port: int) -> socket.SocketIO:
     Args:
         address (str): The address of the destination socket.
         port (int): The port of the destination socket.
+        wait_interval (int): Number of seconds to wait before retrying connection if it fails (default: 5 sec)
 
     Returns:
-        The SocketIO object.
+        TextIO object providing access to opened socket.
     """
+    last_error = None
     while True:
+        if last_error is None:
+            print(f"{time.strftime('%F-%T')} Connecting to {address}:{port} ...", file=sys.stderr)
         try:
             s = socket.create_connection((address, port))
-            sys.stderr.write(time.strftime("%F-%T") +
-                             f" Connection to {address}:{port} established.\n")
-            sys.stderr.flush()
-            return socket.SocketIO(s, "w")
-        except socket.error:
-            # sleep for a while and then reconnect
-            sys.stderr.write(time.strftime("%F-%T") +
-                             " Connection failed, trying again in 5 seconds.\n")
-            sys.stderr.flush()
-            time.sleep(5)
+            print(f"{time.strftime('%F-%T')} Connection established.", file=sys.stderr)
+            last_error = None
+            return s.makefile("w", encoding="utf-8")
+        except OSError as e:
+            # sleep for a while and then reconnect (print error only once or when the error message changes)
+            if last_error is None or str(e) != last_error:
+                print(f"{time.strftime('%F-%T')} Connection failed ({e}), retrying every {wait_interval} seconds ...", file=sys.stderr)
+                last_error = str(e)
+            time.sleep(wait_interval)
 
 
 def main():
@@ -77,16 +80,16 @@ def main():
 
     # Parsing the arguments
     if parsed_args.filename:
-        file = io.FileIO(parsed_args.filename, "w")
+        file = open(parsed_args.filename, "w", encoding="utf-8")
     elif parsed_args.filename_append:
-        file = io.FileIO(parsed_args.filename_append, "a")
+        file = open(parsed_args.filename_append, "a", encoding="utf-8")
     elif parsed_args.networktarget:
-        addr = parsed_args.networktarget.split(":")
-        if len(addr) != 2:
-            raise argparse.ArgumentError(argument=None,
-                                         message="Malformed argument of -s host:port")
-        address = addr[0]
-        port = int(addr[1])
+        try:
+            address, port = parsed_args.networktarget.split(":")
+            port = int(port)
+        except (TypeError, ValueError):
+            print("Error: malformed argument of -s host:port", file=sys.stderr)
+            sys.exit(1)
         file = connect_socket(address, port)
     else:
         file = sys.stdout
@@ -98,15 +101,13 @@ def main():
     # Set JSON as required data type on input
     trap.setRequiredFmt(0, pytrap.FMT_JSON, "")
 
-    stop = False
-    # Main loop (trap.stop is set to True when SIGINT or SIGTERM is received)
-    while not stop:
+    # Main loop
+    while True:
         # Read data from input interface
         try:
             data = trap.recv()
         except pytrap.FormatMismatch:
-            sys.stderr.write(
-                "Error: output and input interfaces data type or format mismatch\n")
+            print("Error: output and input interfaces data type or format mismatch", file=sys.stderr)
             break
         except pytrap.FormatChanged as e:
             if parsed_args.verbose:
@@ -121,26 +122,35 @@ def main():
                 print('Received "end-of-stream" message, going to quit.')
             break
 
+        # Decode data (and check it's valid JSON)
         try:
-            # Decode data (and check it's valid JSON)
             rec = json.loads(data.decode("utf-8"))
-            if parsed_args.verbose:
-                print(f"Message: {format(rec)}")
-            # Print it to file or stdout
-            file.write(bytes(json.dumps(rec, indent=parsed_args.indent) + '\n', "utf-8"))
+        except ValueError as e:
+            print(f"ERROR: Received invalid JSON (message skipped): {e}", file=sys.stderr)
+            continue
+
+        if parsed_args.verbose:
+            print(f"Message: {format(rec)}")
+
+        # Print it to file, stdout, or send to socket
+        try:
+            file.write(json.dumps(rec, indent=parsed_args.indent) + '\n')
             if not parsed_args.noflush:
                 file.flush()
-        except ValueError as e:
-            sys.stderr.write(str(e) + '\n')
-            sys.stderr.flush()
-        except BrokenPipeError:
+        except IOError as e:
             if parsed_args.networktarget:
+                print(f"{time.strftime('%F-%T')} Connection error: {e}", file=sys.stderr)
+                # connection error, try to reconnect and send the message again
                 file = connect_socket(address, port)
-                file.write(
-                    bytes(json.dumps(rec, indent=parsed_args.indent) + '\n', "utf-8"))
+                file.write(json.dumps(rec, indent=parsed_args.indent) + '\n')
                 if not parsed_args.noflush:
                     file.flush()
+            else:
+                raise
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass # quietly exit program without traceback
