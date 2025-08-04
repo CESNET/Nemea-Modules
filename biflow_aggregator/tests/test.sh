ca2869712f13b8952738edc37eaa5adca3b3924a
Damir Zainullin <zainudam@localhost.localdomain>
2025-08-04 16:10:48 +0200

    Biflow Aggregator - Fix pipeline

--- biflow_aggregator/tests/test.sh
+++ biflow_aggregator/tests/test.sh
@@ -14,20 +14,20 @@ run_test_with_global_timeout() {
     local input=$1
     local output=$2
     local reference=$3
-    local config=$4
-    local global_timeout=$5
-
-    (../biflow_aggregator -i "u:lr,u:ba" -e -c config.xml -n $config -g $global_timeout & ) || true
+    local section=$4
+    local config=$5
+    local global_timeout=$6
+    (./biflow_aggregator -i "u:lr,u:ba" -e -c $config -n $section -g $global_timeout & ) || true
     local AGGREGATOR_PID=$!
     sleep 0.5
     die_if_not_running $AGGREGATOR_PID "Failed to start biflow aggregator"
 
-    ../../logger/logger -w $output -i "u:ba" &  
+    ./../logger/logger -w $output -i "u:ba" &  
     local LOGGER_PID=$!
     sleep 0.5
     die_if_not_running $LOGGER_PID "Failed to start logger"
 
-    ../../logreplay/logreplay -f $input -i "u:lr" &  
+    ./../logreplay/logreplay -f $input -i "u:lr" &  
     local LOGREPLAY_PID=$!
     sleep 0.5
     die_if_not_running $LOGREPLAY_PID "Failed to start logreplay"
@@ -39,24 +39,35 @@ run_test_with_global_timeout() {
     kill $LOGGER_PID 2>/dev/null
     wait $LOGGER_PID
     
-    if ! colordiff $output $reference; then
+    if ! diff $output $reference; then
         echo $output doesnt match $reference
         success="false"
     fi
 }
+    pwd >&2
 
 script_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-cd $script_path
+
 success="true"
-mkdir -p outputs 
-for input in inputs/input*; do
+
+tmpdir=$(mktemp -d)
+
+trap "rm -rf \"$tmpdir\"" EXIT
+
+outputs="$tmpdir/outputs"
+
+mkdir -p "$outputs" 
+
+
+for input in "$script_path"/inputs/input*; do
     basename=$(basename "$input")
     echo Found $basename
     if [[ $basename =~ input([0-9]+)_(.+) ]]; then
         index="${BASH_REMATCH[1]}"
-        config="${BASH_REMATCH[2]}"
-        output=outputs/output$index
-        reference=references/reference$index
+        section="${BASH_REMATCH[2]}"
+        output="$outputs/output$index"
+	config="$script_path/config.xml"
+        reference="$script_path/references/reference$index"
     else    
         echo $input
         echo "Incorrect input name. Must be input<index>_<config_name>"
@@ -64,13 +75,13 @@ for input in inputs/input*; do
     fi
 
     echo "Running test without global timeout..."
-    run_test_with_global_timeout $input ${output}_gt0 ${reference}_gt0 $config "0"
+    run_test_with_global_timeout $input ${output}_gt0 ${reference}_gt0 $section $config "0"
 
     echo "Running test with relative global timeout..."
-    run_test_with_global_timeout $input ${output}_gt5r ${reference}_gt5r $config "5r"
+    run_test_with_global_timeout $input ${output}_gt5r ${reference}_gt5r $section $config "5r"
 
     echo "Running test with absolute global timeout..."
-    run_test_with_global_timeout $input ${output}_gt5a ${reference}_gt5a $config "5a"
+    run_test_with_global_timeout $input ${output}_gt5a ${reference}_gt5a $section $config "5a"
 
 done
 
